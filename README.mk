9afd925dc44a2ea7f11ed325be4c2456fd7b69cd
Phillip Rhodes (mindcrime) <motley.crue.fan@gmail.com>
2014-03-25 19:04:39 -0400

    adds README

---
+++ README.mk
@@ -0,0 +1,12 @@
+ArrowSmith - A re-implementation of the classic ArrowSmith Literature Based Discovery
+system, using Groovy and Grails.
+
+This is a learning platform of anyone interested in LBD to play and experiment.  Our
+first goal is to re-implement the classic algorithm(s) that Don Swanson developed, and
+then start looking for new and interesting variations and additions.  
+
+As such, one of our primary goals here is to make this as modular and pluggable as possible, in
+order to facilitate experimenting with novel approaches to Literature Based Discovery.
+
+Code is licensed under the Apache License v2.
+
